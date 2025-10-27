08c893f5b9b6b4e5d5113576efb870af2c613fd5
GOG <GOG@mail.ru>
2025-10-27 16:46:25 +0300

    Added new routes to router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,19 @@
-import App from "../App";
+import AdminView from "../views/adminView";
+import AuthErrorView from "../views/authErrorView";
+import AuthView from "../views/authView";
+import HRManagerView from "../views/hrManagerView";
+import SuperUserView from "../views/superUserView";
+import SupervisorView from "../views/supervisorView";
+import UserView from "../views/userView";
 
 const routes = [
-    {path: '/', component: App},
+    {path: '/Auth', component: AuthView},
+    {path: '/Admin', component: AdminView},
+    {path: '/AuthError', component: AuthErrorView},
+    {path: '/HRManager', component: HRManagerView},
+    {path: '/Superuser', component: SuperUserView},
+    {path: '/Supervisor', component: SupervisorView},
+    {path: '/User', component: UserView},
 ]
 
 export default routes;
