04e9760a03f33d14dd3aa92f71dc48ff19c10083
GOG <GOG@mail.ru>
2025-10-27 15:50:25 +0300

    setted up basic router for project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import routes from './router'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+function Root(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(route=>{
+          return <Route path={route.path} Component={route.component} />
+        })}
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-    <App />,
+    <Root />,
 )
