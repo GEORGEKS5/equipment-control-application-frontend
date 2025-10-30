4221b9a010db77b43d2d3de69c283cdc30981187
GOG <GOG@mail.ru>
2025-10-30 13:23:53 +0300

    added imports to formEquipEdit component

--- src/components/supervisorView/formEquipEdit.jsx
+++ src/components/supervisorView/formEquipEdit.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import equipmentForm from '@/mixins/equipmentForm.js'    
+import getEquipmentListData from '../../helpers/Data/equipmentListDataFetch.js'
+import getRequestPromise from '../../helpers/lib';
+import markSelectedProperty from '../../helpers/markData.js'
+import DialogWindow from "../../layouts/dialogWindow";
+import FormInput from "../UI/formInput.jsx";
+import DefaultButton from "../UI/defaultButton.jsx";
+
+
+import formSelectBlock from '@/components/formSelectBlock.vue';
 
 function FormEquipEdit({}) {
     return(
