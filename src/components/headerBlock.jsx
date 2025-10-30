ba357cbe0389e0e226fd193de58f7060ac3c3e96
GOG <GOG@mail.ru>
2025-10-30 11:37:59 +0300

    created event handlers for headerBlock component

--- src/components/headerBlock.jsx
+++ src/components/headerBlock.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import ChangePassword from "./changePassword";
+import getRequestPromise from "../helpers/lib";
+import {useNavigate} from "react-router";
+import UserContext from "../context/user";
 
 function HeaderBlock({}) {
+    const navigateTo = useNavigate();
+    const [USER_STATE] = useContext(UserContext);
+    const [changePasswordFormVisible, setChangePasswordFormVisible] = useState(false);
+
+
+    async function shutdown(){
+        try{
+            await getRequestPromise(USER_STATE.getServerUrlAddress(), 'Logout');
+        }catch(e){
+            console.error(e);
+        }
+
+        navigateTo('/auth')
+    }
+
+    function showChangePasswordForm(){
+        setChangePasswordFormVisible(true);
+    }
+
+    function hideChangePasswordForm(){
+        setChangePasswordFormVisible(false);
+    }
+
     return (
         <>
         
