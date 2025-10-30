cde5b5472214515bc56e062ccd46613e38d0f4d5
GOG <GOG@mail.ru>
2025-10-30 14:51:53 +0300

    adapted contentBlockSplitSection component

--- src/layouts/contentBlockSplitSection.jsx
+++ src/layouts/contentBlockSplitSection.jsx
@@ -1,7 +1,19 @@
+import SectionHeader from './slots/sectionHeader';
+import SectionFooter from './slots/sectionFooter';
 
 function ContentBlockSplitSection({children}) {
+    const upperSection = React.Children.toArray(children).find(child => child.type === SectionHeader);
+    const lowerSection = React.Children.toArray(children).find(child => child.type === SectionFooter);
+
     return (
-        <></>
+        <div className="grid grid-rows-[40vh_40vh]">
+            <div id="upperSection">
+                {upperSection}
+            </div>
+            <div id="lowerSection">
+                {lowerSection}
+            </div>
+        </div>
     )   
 }
 
