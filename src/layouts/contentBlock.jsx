178f1f3e769b772ddf895f381aaac52b56aa258d
GOG <GOG@mail.ru>
2025-10-30 14:44:59 +0300

    adapted contentBlock component

--- src/layouts/contentBlock.jsx
+++ src/layouts/contentBlock.jsx
@@ -1,9 +1,19 @@
+import MainContent from './slots/mainContent';
+import SideBar from './slots/sideBar';
 
-function ContentBlock() {
+function ContentBlock({children}) {
+    const mainContent = React.Children.toArray(children).find(child => child.type === MainContent);
+    const sideBar = React.Children.toArray(children).find(child => child.type === SideBar);
+
     return(
-        <>
-        
-        </>
+        <div id="contentRoot" className="flex flex-col xl:flex-row justify-evenly bg-[#07070D] w-[100%] p-2">
+            <div id="mainContent" className="flex flex-col w-full xl:w-[60%] bg-[#F3F3F3] h-[87vh] p-3">
+                {mainContent}
+            </div>
+            <div id="sideContent" className="grid grid-col w-full xl:w-[38%] bg-[#F3F3F3] h-[87vh] p-3">
+                {sideBar}
+            </div>
+        </div>
     )
 }
 
