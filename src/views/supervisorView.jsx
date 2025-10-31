97e00c48836fca12af5e097476cd4f022e97a9a8
GOG <GOG@mail.ru>
2025-10-31 10:07:02 +0300

    created basic layout in supervisorView component

--- src/views/supervisorView.jsx
+++ src/views/supervisorView.jsx
@@ -1,8 +1,72 @@
+import HeaderBlock from '../components/headerBlock';
+import DataTable from '../components/table/dataTable';
+
+import ContentBlock from '../layouts/contentBlock';
+import ContentBlockSection from '../layouts/contentBlockSection';
+import ContentBlockSplitSection from '../layouts/contentBlockSplitSection';
+import ContentSection from '../layouts/contentSection';
+import ContentSplitSection from '../layouts/contentSplitSection';
+
+import MainContent from '../layouts/slots/mainContent';
+import SideBar from '../layouts/slots/sideBar';
+import SectionData from "../layouts/slots/sectionData";
+import SectionHeader from "../layouts/slots/sectionHeader";
+import SectionFooter from "../layouts/slots/sectionFooter";
+
+import useSupervisorRepository from '../hooks/useSupervisorRepository';
+import useSupervisorViewDataTable from '../hooks/useSupervisorViewDataTable';
+
 function SupervisorView(){
+    const {supervisorPinedEquipment, pinReadyEquipment, getPinedEquipmentRepository, getPinReadyEquipmentRepository, getUnitedRepository} = useSupervisorRepository();
+    const {pinReadyEquipmentTable, supervisorPinedEquipmentTable} = useSupervisorViewDataTable();
+
+    console.log(pinReadyEquipmentTable);
+
     return (
-        <>
-            <h1>SupervisorView</h1>
-        </>
+        <div id="rootEl" className="flexParent">
+            <HeaderBlock></HeaderBlock>
+
+            <ContentBlock>
+                <MainContent>
+                    <ContentBlockSection>
+                        <ContentSection>
+                            <SectionHeader>
+                                <h3 id="contentHeader">Закреплено</h3>
+                            </SectionHeader>
+                            <SectionData>
+                                <DataTable
+                                    tableData={supervisorPinedEquipment}
+                                    tableStructure={supervisorPinedEquipmentTable.header}
+                                ></DataTable>
+                            </SectionData>
+                            <SectionFooter>
+                                <h4>Закрепить оборудование по SN</h4>
+                            </SectionFooter>
+                        </ContentSection>
+                    </ContentBlockSection>
+                </MainContent>
+
+                <SideBar>
+                    <ContentBlockSection>
+                        <ContentSection>
+                            <SectionHeader>
+                                <h3 id="contentHeader">Доступно</h3>
+                            </SectionHeader>
+                            <SectionData>
+                                <DataTable
+                                    tableData={pinReadyEquipment}
+                                    tableStructure={pinReadyEquipmentTable.header}
+                                    tableActionButton={pinReadyEquipmentTable.button}
+                                ></DataTable>
+                            </SectionData>
+                            <SectionFooter>
+                                <h4>Добавить новое</h4>
+                            </SectionFooter>
+                        </ContentSection>
+                    </ContentBlockSection> 
+                </SideBar>  
+            </ContentBlock>
+        </div>
     )
 }
 
