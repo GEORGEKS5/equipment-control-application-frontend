186b2d845f6165c3bac56bb961da70a252d94544
GOG <GOG@mail.ru>
2025-10-31 10:48:20 +0300

    added icon component, edited tableButton class in buttonStyles for defaultButton component

--- src/components/UI/defaultButton.jsx
+++ src/components/UI/defaultButton.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import Icon from '@mdi/react';
 import btnStyle from '../../styles/button.module.css'
 
-function DefaultButton({buttonValue, buttonClick, buttonClass, buttonCaption}){
+function DefaultButton({buttonValue, buttonClick, buttonClass, buttonCaption, buttonIconPath}){
     const className = btnStyle.defaultButton + ' ' + (btnStyle[buttonClass] ?? '');
 
     return(
         <button value={buttonValue} onClick={buttonClick}  className={className}>
-            <span>{ buttonCaption }</span> 
+            {
+                    buttonIconPath
+                ?
+                    <Icon path={buttonIconPath} size={0.6} style={{transform: 'none'}}></Icon>
+                :
+                    <span>{ buttonCaption }</span>
+            }
         </button>
     )
 }
