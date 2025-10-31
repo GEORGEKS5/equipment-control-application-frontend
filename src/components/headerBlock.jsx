fd464ca7f5a1e1d8782405ed1db1943fdf8861c5
GOG <GOG@mail.ru>
2025-10-31 10:51:03 +0300

    added icon for headerBlock component

--- src/components/headerBlock.jsx
+++ src/components/headerBlock.jsx
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import ChangePassword from "./changePassword";
 import getRequestPromise from "../helpers/lib";
 import {useNavigate} from "react-router";
 import UserContext from "../context/user";
+import Icon from "@mdi/react";
+import {mdiExitToApp, mdiKeyArrowRight, mdiAccountCheck} from '@mdi/js';
 
 function HeaderBlock({}) {
     const navigateTo = useNavigate();
-    const [USER_STATE] = useContext(UserContext);
+    const {USER_STATE} = useContext(UserContext);
     const [changePasswordFormVisible, setChangePasswordFormVisible] = useState(false);
 
     async function shutdown(){
@@ -36,7 +38,7 @@ function HeaderBlock({}) {
             <header className="bg-[#A6775B] text-[#D0D8D9] md:min-w-[650px] md:p-5 py-3">
                 <ul id="header__wrapper" className="flex flex-row justify-evenly md:justify-center content-center">
                     <li id="header__icon" className="min-w-4 md:px-4 rounded-md bg-[#D0D8D9] text-[#07070D] p-1">
-                        {pathAccoutCheck ? <svgIcon type="mdi" className="" path={pathAccountCheck}></svgIcon> : <></>}
+                        <Icon path={mdiAccountCheck} size={1}></Icon>
                     </li>
                     <ul id="header__userBlock" className="flex flex-row justify-evenly md:min-w-[42%] md:mx-5">
                         <li className="min-w-4 md:px-6 leading-5 rounded-sm bg-amber-900 p-1 md:rounded-full text-xs md:text-sm">{USER_STATE.userName}</li>
@@ -46,8 +48,8 @@ function HeaderBlock({}) {
                         {USER_STATE.appointmentDate ? <li className="min-w-4 lg:px-6 md:px-5 leading-5 rounded-sm bg-amber-900 p-1 md:rounded-full text-xs md:text-sm">{USER_STATE.appointmentDate}</li> : <></>}
                     </ul>
                     <ul id="header__AccentBar" className="flex flex-row justify-between min-w-[10%]">
-                        <li className="flex md:min-w-3 md:px-4 rounded-md bg-black"><button click={shutdown}>{pathExitToApp ? <svgIcon type="mdi" path="pathExitToApp"></svgIcon> : <span>Exit</span>}</button></li>
-                        <li className="flex md:min-w-3 md:px-4 rounded-md bg-black"><button click={showChangePasswordForm}>{pathKeyArrowRight ? <svgIcon type="mdi" path="pathKeyArrowRight"></svgIcon> : <span>PSW CHNG</span>}</button></li>
+                        <li className="flex md:min-w-3 md:px-4 rounded-md bg-black"><button click={shutdown}>{mdiExitToApp ? <Icon path={mdiExitToApp} size={1}></Icon> : <span>Exit</span>}</button></li>
+                        <li className="flex md:min-w-3 md:px-4 rounded-md bg-black"><button click={showChangePasswordForm}>{mdiKeyArrowRight ? <Icon path={mdiKeyArrowRight} size={1}></Icon> : <span>PSW CHNG</span>}</button></li>
                     </ul>
                 </ul>
             </header>
