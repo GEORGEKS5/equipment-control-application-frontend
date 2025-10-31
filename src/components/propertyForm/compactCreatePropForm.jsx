c2101102cdda163b8778c9fed5c6371bdc6b3849
GOG <GOG@mail.ru>
2025-10-31 11:38:23 +0300

    adapted compactCreatePropForm component

---
+++ src/components/propertyForm/compactCreatePropForm.jsx
@@ -0,0 +1,100 @@
+import CompactForm from '../../layouts/compactForm';
+import DefaultButton from '../UI/defaultButton';
+import FormInput from '../UI/FormInput.vue';
+import getRequestPromise from '../../helpers/lib';
+import { useContext, useEffect, useState } from 'react';
+import UserContext from '../../context/user';
+import SectionHeader from '../../layouts/slots/sectionHeader';
+import SectionData from '../../layouts/slots/sectionData';
+import SectionFooter from '../../layouts/slots/sectionFooter';
+
+function CompactCreatePropForm({compactFormVisible, targetModelName, targetPropModelName, extraRequestData, selectPropData, identificatorKeyName, valueKeyName, hideCompactForm, extraPropInsert, propInsert}) {
+    const [fieldValue, setFieldValue] = useState('');
+    const [USER_STATE] = useContext(UserContext);
+
+    function hideForm(){
+        hideCompactForm();
+    }
+
+    function changeInputValue(value){
+        setFieldValue(value);
+    }
+
+    function changeSelectValue(value){
+        setFieldValue(value);
+    }
+
+    function propInsert(value){
+        extraPropInsert(value);
+    }
+
+    function saveProp(){
+        let servAdr = USER_STATE.getServerUrlAddress();
+        let endpointName = 'Insert' + targetModelName;
+        
+        let insertObject = {
+            [targetModelName+'Name']: fieldValue,
+        }
+        
+        for(let k in extraRequestData){
+            insertObject[k] = extraRequestData[k];
+        }
+
+        let reqPromise = getRequestPromise(servAdr, endpointName, insertObject);
+
+        reqPromise.then(result=>{
+            if(result.ok){
+                propInsert(fieldValue);
+            }
+
+            setFieldValue('');
+        });
+    }
+
+    useEffect(()=>{
+        let fieldName = 'selected';
+
+        if(fieldName in selectPropData[0]){
+            let sq = selectPropData.find(item => item[fieldName]);
+            
+            if(sq){
+                setFieldValue(sq[this.identificatorKeyName]);
+            }else{
+                console.warn('Not found selected element on selecPropData change');
+            }
+        }else{
+            console.log('Selected false')
+            setFieldValue(selectPropData[0][identificatorKeyName]);
+        }
+
+        console.log(fieldValue);
+    }, [selectPropData])
+    
+    return (
+        <CompactForm
+            formVisible={compactFormVisible}
+            hideForm={hideForm}
+            >
+            <SectionHeader>
+                <h3>Добавление свойства</h3>
+            </SectionHeader>
+            <SectionData>
+                <FormInput 
+                    inputValue={fieldValue}
+                    fieldCaption="Значение"
+                    updateInput={changeInputValue}
+                    >
+                </FormInput>  
+            </SectionData>
+            <SectionFooter>
+                <DefaultButton
+                    buttonCaption="Сохранить"
+                    buttonClass="form"
+                    buttonClick={saveProp}
+                ></DefaultButton>
+            </SectionFooter>
+        </CompactForm>
+    )
+}
+
+export default CompactCreatePropForm;
