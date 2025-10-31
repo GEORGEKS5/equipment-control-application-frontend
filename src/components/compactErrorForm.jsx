ef76c5e40af5260a75da93a48489e40683d2169d
GOG <GOG@mail.ru>
2025-10-31 11:57:58 +0300

    adapted template, methods compactErrorForm component, and created form style module

--- src/components/compactErrorForm.jsx
+++ src/components/compactErrorForm.jsx
@@ -1,6 +1,26 @@
+import formStyle from '../styles/form.module.css';
 
-function CompactErrorForm({}) {
+function CompactErrorForm({compactFormDescription, compactFormVisible, closeButtonPress}) {
+
+    function closeButtonPress(){
+        closeButtonPress();
+    }
+   
     return (
+            compactFormVisible
+        ?
+            <div className={formStyle.error__formWrapper} onClick={closeButtonPress}>
+                <div className={formStyle.error__formContent}>
+                    <div className={formStyle.error__fieldsWrapper} onClick={e => {e.stopPropagation()}}>
+                        <h3>Ошибка</h3>
+                        <div className={formStyle.error__activeWrapper}>
+                            <p>{ compactFormDescription }</p>
+                            <button onClick={closeButtonPress}>Понятно</button>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        :
         <></>
     )
 }
