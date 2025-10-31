5fe3776e8eaf143250f6ecab1acecfa88a338ea0
GOG <GOG@mail.ru>
2025-10-31 16:39:48 +0300

    created window closing method for dialogWindow component

--- src/layouts/dialogWindow.jsx
+++ src/layouts/dialogWindow.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import FormField from './slots/formField';
 import FormFooter from './slots/formFooter';
 
-function DialogWindow({visibleForm, children}){
+function DialogWindow({visibleForm, children, hideForm}){
 
     const formFields = React.Children.toArray(children).find(child => child.type === FormField);
     const formFooter = React.Children.toArray(children).find(child => child.type === FormFooter);
 
     function hideWindow(){
-        console.log('hide form');
-    }
-
-    function stopProp(e){
-        e.stopPropogation();
-        console.log('stop propog');
+        hideForm();
     }
 
     return (
         (visibleForm 
             ?
             <div id="formBackround" className="fixed inset-y-0 inset-x-0 flex justify-center overflow-hidden bg-[#ffffffd9] z-[9999]" onClick={hideWindow}>
-                <div onClick={stopProp} id="formContent" className="bg-[#ffffff] p-0 min-w-[325px] md:min-w-[500px] mx-auto min-h-[610px] shadow-md rounded-md grid grid-rows-[0.25fr_0.50fr_auto] md:grid-rows-[0.15fr_0.70fr_auto]">
+                <div onClick={e => e.stopPropagation()} id="formContent" className="bg-[#ffffff] p-0 min-w-[325px] md:min-w-[500px] mx-auto min-h-[610px] shadow-md rounded-md grid grid-rows-[0.25fr_0.50fr_auto] md:grid-rows-[0.15fr_0.70fr_auto]">
                     <div id="formHeader" class="flex flex-col justify-center">
                         <h1 className="text-center text-2xl text-[#000000]">Форма</h1>
                     </div>
