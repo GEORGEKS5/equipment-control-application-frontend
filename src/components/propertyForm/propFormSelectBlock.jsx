38bc9d8371f2eb8becbb53bff1251b1681d3e986
GOG <GOG@mail.ru>
2025-10-31 11:10:58 +0300

    added imports for propFormSelectBlock component

--- src/components/propertyForm/propFormSelectBlock.jsx
+++ src/components/propertyForm/propFormSelectBlock.jsx
@@ -1,3 +1,9 @@
+import CompactEditForm from '../compactEditForm';
+import CompactCreateForm from '../compactCreatePropForm';
+import CompactErrorForm from '../compactErrorForm';
+
+import FormSelect from '../UI/propertyFormSelect';
+import DefaultButton from '../UI/defaultButton';
 
 function PropFormSelectBlock({}) {
     return (
