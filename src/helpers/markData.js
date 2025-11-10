eb0b22b250ba7a5ddf40eb9313e2bd4cfe4c5e57
GOG <GOG@mail.ru>
2025-11-10 23:31:57 +0300

    added array check in markData helper

--- src/helpers/markData.js
+++ src/helpers/markData.js
@@ -2,9 +2,10 @@
  * Добавляет в исходный массив поле по которому можно отметить текущее значение
  * @param {Array} selectArray Массив на основе которого строится выпадающий список
  * @param {String} value Выбраное значение
+ * @param {String} key Имя ключевое поле в массиве
  */
 function markSelectedProperty(selectArray, value, key){
-    if(selectArray){
+    if(Array.isArray(selectArray)){
         selectArray.map(item => {
             item.selected = ''+item[key] === ''+value ? true : false
             //console.log(item[key] + ' ' + value + ' ' + item.selected +' '+ typeof item[key] + ' ' + typeof value);
