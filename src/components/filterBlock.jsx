b52564e621d616983de5f8f1bc077f1b03761cfe
GOG <GOG@mail.ru>
2025-11-11 13:15:29 +0300

    created block style module, adapted filterBlock component

--- src/components/filterBlock.jsx
+++ src/components/filterBlock.jsx
@@ -1,7 +1,50 @@
+import { useMemo } from "react";
+import blockStyle from '../styles/block.module.css';
 
-function FilterBlock({}) {
+function FilterBlock({originFilterObject = [], orderCategory = '', elementOrdered}) {
+    
+    function filterElements(evt){
+        let filterValue = evt.target.value;
+        let filteredElements = [];
+
+        filteredElements = originFilterObject.filter(item => {
+            let currentString = String(item[orderCategory]); 
+            return currentString == filterValue;
+        });
+
+        elementOrdered(filteredElements);
+    }
+
+    const filterOptions = useMemo(()=>{
+        console.log('filterOprionMemo on change originFilterObject');
+        let set = new Set();
+
+        if(originFilterObject.length){
+            originFilterObject.forEach(item => {
+            set.add(item[orderCategory])
+        });
+        }
+
+        return set;
+    },[originFilterObject])
+
     return (
-        <></>
+        <div className="grid grid-rows-[0.25fr_0.7fr] grid-cols-[0.8fr] justify-center overflow-hidden">
+            <h3>Фильтрация</h3>
+            <div className={blockStyle.listWrapper}>
+                <ul>
+                    {
+                        filterOptions.map((item) => {
+                            return (
+                                <li>
+                                    <button className="bg-[#ffffff] text-[#000000]" onClick={filterElements} value={item}>{ item }</button>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </div>
     )
 }
 
