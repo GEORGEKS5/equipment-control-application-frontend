22703a6c3f6a377337c5dd5e35571f298b90ba81
GOG <GOG@mail.ru>
2025-11-11 13:32:33 +0300

    adapted sortBlock component

--- src/components/sortBlock.jsx
+++ src/components/sortBlock.jsx
@@ -1,8 +1,59 @@
-function SortBlock({}) {
+import { useMemo } from "react";
+import DefaultButton from "./UI/defaultButton";
+
+function SortBlock({originSortObject = [], orderCategory = '', elementOrdered}) {
+    function emitSortingElementsByASC(){
+        elementOrdered(getSortedPinReadyEquipmentByASC);
+    }
+
+    function emitSortingElementsByDESC(){
+        elementOrdered(getSortedPinReadyEquipmentByDESC);
+    }
+
+    function sortByASC(a,b){
+        const aString = String(a[this.orderCategory]);
+        const bString = String(b[this.orderCategory]);
+
+        return aString.localeCompare(bString)
+    }
+
+    function sortByDESC(a,b){
+        const aString = String(a[this.orderCategory]);
+        const bString = String(b[this.orderCategory]);
+
+        return bString.localeCompare(aString)
+    }
+
+    const getSortedPinReadyEquipmentByASC = useMemo(()=>{
+        if(originSortObject.length){
+            return [...originSortObject].sort( sortByASC );
+        }
+        return [];
+    }, [originSortObject]);
+    
+    const getSortedPinReadyEquipmentByDESC = useMemo(()=>{
+        if(originSortObject.length){
+            return [...originSortObject].sort( sortByDESC );
+        }
+        return [];
+    }, [originSortObject]);
+
     return (
-        <>
-        
-        </>
+        <div className="grid grid-cols-[0.8fr 1fr] items-center">
+            <h3 style={{ borderRight: "1px solid black", fontSize: '10px'}}>Сортировка</h3>
+            <div className="flex justify-evenly items-center">
+                <DefaultButton
+                    buttonCaption="А"
+                    buttonClass="default"
+                    buttonClick={emitSortingElementsByASC}
+                ></DefaultButton>
+                <DefaultButton
+                    buttonCaption="'Я'"
+                    buttonClass="'default'"
+                    buttonClick={emitSortingElementsByDESC}
+                ></DefaultButton>
+            </div>    
+        </div>
     )
 }
 
