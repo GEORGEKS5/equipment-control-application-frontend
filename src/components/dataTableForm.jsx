5c578079acc756ccc464a68c0fcf8d13fe295146
GOG <GOG@mail.ru>
2025-11-12 13:46:35 +0300

    adapted dataTableForm component

--- src/components/dataTableForm.jsx
+++ src/components/dataTableForm.jsx
@@ -1,7 +1,19 @@
+import DialogWindow from '../layouts/dialogWindow';
+import DataTable from '../components/dataTable';
+import FormField from '../layouts/slots/formField';
 
-function DataTableForm({}) {
+function DataTableForm({formVisible, tableData, tableHeader, hideForm}) {
     return (
-        <></>
+        <DialogWindow 
+            visibleForm={formVisible}
+            hideWindow={hideForm}>
+            <FormField>
+                <DataTable
+                    tableData={tableData}
+                    tableStructure={tableHeader}>
+                </DataTable>
+            </FormField>
+        </DialogWindow>
     )
 }
 
