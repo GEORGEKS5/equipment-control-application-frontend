981dd59b5082d9ae9d969f48bdd6ba7b0c294971
GOG <GOG@mail.ru>
2025-11-12 11:11:45 +0300

    adapted dataOrderForm component

--- src/components/dataOrderForm.jsx
+++ src/components/dataOrderForm.jsx
@@ -1,7 +1,69 @@
+import SortBlock from "./sortBlock";
+import FilterBlock from "./filterBlock";
+import DefaultButton from "./UI/defaultButton";
+import { useEffect, useState } from "react";
+import blockStyle from '../styles/block.module.css';
 
-function DataOrderForm({params}) {
+function DataOrderForm({clientRect, formVisible, originFilterObject, originSortObject, orderCategory, elementOrdered, hideForm}) {
+    const [filterFormTopOffset, setFilterFormTopOffset] = useState(0);
+    const [filterFormLeftOffset, setFilterFormLeftOffset] = useState(0);
+
+    function resetFilter(){
+        elementOrdered(originFilterObject);
+    }
+
+    function setAbsoluteFormOffset(){
+        const customOffset = 3;
+
+        setFilterFormTopOffset(getAbsoluteFormTopOffset(clientRect[0], customOffset)); 
+        setFilterFormLeftOffset(getAbsoluteFormLeftOffset(clientRect[0], customOffset));
+    }
+
+    function getAbsoluteFormTopOffset(clientRect, customOffset){
+        return clientRect.top + customOffset + clientRect.height + window.pageYOffset;
+    }
+
+    function getAbsoluteFormLeftOffset(clientRect, customOffset){
+        return clientRect.left + customOffset + window.pageXOffset
+    }
+    
+    useEffect(() => {
+        if(clientRect){
+            
+            setAbsoluteFormOffset();
+        }
+    }, [clientRect]);
+
     return(
-        <></>
+                formVisible 
+            ?
+                <div className={blockStyle.filterWrapper} style={{top: `${filterFormTopOffset}px`, left: filterFormLeftOffset + 'px'}}>
+                    <SortBlock
+                        originSortObject={originSortObject}
+                        orderCategory={orderCategory}
+                        elementOrdered={elementOrdered}
+                    ></SortBlock>
+                    <FilterBlock
+                        originFilterObject={originFilterObject}
+                        orderCategory={orderCategory}
+                        elementOrdered={elementOrdered}
+                    ></FilterBlock>
+
+                    <div id="buttonBlock">
+                        <DefaultButton
+                            buttonCaption="Сброс"
+                            buttonClass="defaultSM"
+                            buttonClick={resetFilter}
+                        ></DefaultButton>
+                        <DefaultButton
+                            buttonCaption="Закрыть"
+                            buttonClass="actionSM"
+                            buttonClick={hideForm}
+                        ></DefaultButton>
+                    </div>
+                </div>
+            :
+                <></>
     )
 }
 
