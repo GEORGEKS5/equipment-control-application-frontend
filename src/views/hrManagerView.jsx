23ecd88701092bf67b4d77d43ac05cbe3ca54945
GOG <GOG@mail.ru>
2025-11-12 13:10:26 +0300

    added headerBlock to hrManagerView

--- src/views/hrManagerView.jsx
+++ src/views/hrManagerView.jsx
@@ -1,6 +1,9 @@
+import HeaderBlock from "../components/headerBlock";
+
 function HRManagerView(){
     return (
         <>
+            <HeaderBlock />
             <h1>HR manager view</h1>
         </>
     )
