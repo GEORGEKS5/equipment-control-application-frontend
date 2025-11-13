147079dbf1c1c8313ed78cc514a71a8fdb33f658
GOG <GOG@mail.ru>
2025-11-13 21:43:02 +0300

    added components, layouts, hooks imports to HrManagerView

--- src/views/hrManagerView.jsx
+++ src/views/hrManagerView.jsx
@@ -1,4 +1,18 @@
 import HeaderBlock from "../components/headerBlock";
+import dataTable from "@/components/dataTable.vue";
+import contentSectionFooter from "@/components/contentSectionFooter.vue";
+import dataOrderForm from "@/components/dataOrderForm.vue";
+
+import contentSingleBlock from "../layouts/contentBlock.vue";
+import contentBlockSection from "../layouts/contentBlockSection.vue";
+import contentSection from "../layouts/contentSection.vue";
+
+import useFullEmployeeRepository from '@/composables/HRManagerView/useFullEmployeeRepository.js';
+import useHRManagerViewDataTable from "@/composables/HRManagerView/useHRManagerViewDataTable";
+import useHRManagerFormModel from "@/composables/HRManagerView/useHRManagerFormModel";
+import useFilterEmployeeModel from '@/composables/HRManagerView/useFilterEmployeeModel.js';
+
+import { FilterForm } from "@/helpers/classes";
 
 function HRManagerView(){
     return (
