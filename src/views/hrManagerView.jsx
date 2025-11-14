8302ab89ad68b368a1225b5bdb25342619164f12
GOG <GOG@mail.ru>
2025-11-14 23:36:48 +0300

    remarked imports, used hrManagerViewDataTable, fullEmployeeRepository, filterEmployeeModel hooks in hrManagerView

--- src/views/hrManagerView.jsx
+++ src/views/hrManagerView.jsx
@@ -1,20 +1,35 @@
 import HeaderBlock from "../components/headerBlock";
-import dataTable from "@/components/dataTable.vue";
-import contentSectionFooter from "@/components/contentSectionFooter.vue";
-import dataOrderForm from "@/components/dataOrderForm.vue";
+import DataTable from "../components/table/dataTable.jsx";
+import DataOrderForm from "../components/dataOrderForm.jsx";
 
-import contentSingleBlock from "../layouts/contentBlock.vue";
-import contentBlockSection from "../layouts/contentBlockSection.vue";
-import contentSection from "../layouts/contentSection.vue";
+import ContentSingleBlock from "../layouts/contentBlock";
+import ContentBlockSection from "../layouts/contentBlockSection";
+import ContentSection from "../layouts/contentSection";
+import ContentSectionFooter from "../layouts/slots/contentSectionFooter.jsx";
 
-import useFullEmployeeRepository from '@/composables/HRManagerView/useFullEmployeeRepository.js';
-import useHRManagerViewDataTable from "@/composables/HRManagerView/useHRManagerViewDataTable";
-import useHRManagerFormModel from "@/composables/HRManagerView/useHRManagerFormModel";
-import useFilterEmployeeModel from '@/composables/HRManagerView/useFilterEmployeeModel.js';
+import useFullEmployeeRepository from '../hooks/hrmanager/useFullEmployeeRepository.js';
+import useHRManagerViewDataTable from "../hooks/hrmanager/useHRManagerViewDataTable";
+import useHRManagerFormModel from "../hooks/hrmanager/useHRManagerFormModel";
+import useFilterEmployeeModel from '../hooks/hrmanager/useFilterEmployeeModel.js';
 
-import { FilterForm } from "@/helpers/classes";
+import { FilterForm } from "../helpers/classes";
+import { useEffect } from "react";
 
 function HRManagerView(){
+    const {appointReadyEmployeeTable, appointedEmployeeTable, createAppointReadyEmployeeTable, createAppointedEmployeeTable} = useHRManagerViewDataTable();
+    const {appointedEmployeesInitialRepository, appointmentReadyEmployeesInitialRepository, getAppointedEmployeeRepository, getAppointmentReadyEmployeeRepository} = useFullEmployeeRepository();
+    const {filterAppointReadyEmployee, filterAppointedEmployee} = useFilterEmployeeModel(appointedEmployeesInitialRepository, appointmentReadyEmployeesInitialRepository);
+
+    useEffect(()=>{
+        createAppointReadyEmployeeTable();
+        createAppointedEmployeeTable();
+
+        getAppointedEmployeeRepository().then(val => {
+            return getAppointmentReadyEmployeeRepository();
+        });
+
+    }, []);
+
     return (
         <>
             <HeaderBlock />
