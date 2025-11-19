1eca71657e4cec69b3f9b59d18e0cd786f7222ee
GOG <GOG@mail.ru>
2025-11-19 22:23:31 +0300

    adapted employeeCreateForm component for hrManagerView

--- src/components/hrmanagerView/employeeCreate.jsx
+++ src/components/hrmanagerView/employeeCreate.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import EmployeeCommon from '../hrmanagerView/employeeCommon'
+import AddressCreateForm from '../hrmanagerView/addressCreateForm';
+import FormButtonBlock from '../UI/formButtonBlock';
+import DefaultSlot from "../../layouts/slots/defaultSlot";
 
-function EmployeeCreate({}) {
+function EmployeeCreate({formVisible, hideForm, employeeRegistred}) {
+    const [addressCreateFormVisible, setAddressCreateFormVisible] = useState(false);
+    const [houseRequestModel, setHouseRequestModel] = useState({});
+    
+    function clearHouseRequestModel() {
+        setHouseRequestModel({
+            regionName: '',
+            cityName: '',
+            typeName: '',
+            streetName: '',
+            houseNumber: 0,
+            houseId: 0,
+            apartmentNumber: 0,
+        });
+    }
+
+    function showAddressCreateForm(){
+        setAddressCreateFormVisible(true);
+    }
+
+    function hideAddressCreateForm(){
+        setAddressCreateFormVisible(false);
+    }
+
+    function setHouseModel(houseModel){
+        hideAddressCreateForm();
+        setHouseRequestModel(()=>{
+            return {...houseModel};
+        });
+    }
+
     return (
-        <></>
+        <>
+            <EmployeeCommon
+                formVisible={formVisible}
+                employeeRegistEndPointName="RegistEmployee"
+                houseRequestModel={houseRequestModel}
+                userNameFieldDisplayOnly={false}
+                hideForm={hideForm}
+                employeeRegistred={() => {employeeRegistred()}}
+                clearHouseRequestModel={clearHouseRequestModel}>
+                    <DefaultSlot>
+                        <FormButtonBlock
+                            fieldCaption="Адрес"
+                            buttonCaption="Выбрать"
+                            buttonClick={showAddressCreateForm}>
+                        </FormButtonBlock>
+                    </DefaultSlot>
+            </EmployeeCommon>
+
+            <AddressCreateForm
+                formVisible={addressCreateFormVisible}
+                externalAddressRequestModel={houseRequestModel}
+                addressCreateEndPointName="InsertHouse"
+                hideForm={hideAddressCreateForm}
+                houseCreated={setHouseModel}>
+            </AddressCreateForm>
+        </>
     )   
 }
 
