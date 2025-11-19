4088138fe8d001bc5b2356089f4d94bcf48896e5
GOG <GOG@mail.ru>
2025-11-19 21:50:11 +0300

    changed className attr instead class for formHeader in dialogWindow component

--- src/layouts/dialogWindow.jsx
+++ src/layouts/dialogWindow.jsx
@@ -16,7 +16,7 @@ function DialogWindow({visibleForm, children, hideForm}){
             ?
             <div id="formBackround" className="fixed inset-y-0 inset-x-0 flex justify-center overflow-hidden bg-[#ffffffd9] z-[9999]" onClick={hideWindow}>
                 <div onClick={e => e.stopPropagation()} id="formContent" className="bg-[#ffffff] p-0 min-w-[325px] md:min-w-[500px] mx-auto min-h-[610px] shadow-md rounded-md grid grid-rows-[0.25fr_0.50fr_auto] md:grid-rows-[0.15fr_0.70fr_auto]">
-                    <div id="formHeader" class="flex flex-col justify-center">
+                    <div id="formHeader" className="flex flex-col justify-center">
                         <h1 className="text-center text-2xl text-[#000000]">Форма</h1>
                     </div>
                     <div id="formFieldWrapper" className="flex flex-col overflow-hidden justify-evenly p-8 md:px-20">
