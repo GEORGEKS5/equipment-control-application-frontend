2ea0e7d4454773e105d10728cef800b7664a0876
GOG <GOG@mail.ru>
2025-11-20 13:15:58 +0300

    adapted useEmployeeAppointRepository hook

--- src/hooks/useEmployeeAppointRepository.js
+++ src/hooks/useEmployeeAppointRepository.js
@@ -1,31 +1,31 @@
-import getRequestPromise from "@/helpers/lib";
-import { ref, watch } from "vue";
-import { useStore } from "vuex";
+import { useContext, useState } from "react";
+import getRequestPromise from "../helpers/lib";
+import UserContext from "../context/user";
 
 export default function(){
-    const positionRepository = ref({});
-    const departmentRepository = ref({});
+    const [positionRepository, setPositionRepository] = useState({});
+    const [departmentRepository, setDepartmentRepository] = useState({});
 
-    const store = useStore();
+    const {USER_STATE} = useContext(UserContext);
     
     const getPositionRepo = async function(v){
-        const servAdr = store.getters.SERVERURLADDRESS;
+        const servAdr = USER_STATE.getServerUrlAddress();
         const endPoint = 'GetPosition';
 
         const requestPromise = await getRequestPromise(servAdr, endPoint);
         const jsonResponse = await requestPromise.json();
 
-        positionRepository.value = jsonResponse;
+        setPositionRepository(jsonResponse);
     };
 
     const getDepartmentRepo = async function(v){
-        const servAdr = store.getters.SERVERURLADDRESS;
+        const servAdr = USER_STATE.getServerUrlAddress();
         const endPoint = 'GetDepartment';
 
         const reqPromise = await getRequestPromise(servAdr, endPoint, {});
         const respObject = await reqPromise.json();
 
-        departmentRepository.value = respObject;
+        setDepartmentRepository(respObject);
     };
 
     return{
