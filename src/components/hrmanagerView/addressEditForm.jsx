22567bb00554157fc48d3db36f74d281f2f902bf
GOG <GOG@mail.ru>
2025-11-21 20:27:24 +0300

    adapted addressEditForm component

--- src/components/hrmanagerView/addressEditForm.jsx
+++ src/components/hrmanagerView/addressEditForm.jsx
@@ -1,6 +1,18 @@
-function AddressEditForm({}) {
+import AddressCreateForm from '../../components/hrmanagerView/addressCreateForm';
+import useAddressModelRepository from '../../hooks/hrmanager/useAddressModelRepository';
+
+function AddressEditForm({formVisible, activeEmployeeUserName, hideForm}) {
+    const {addressModel, hideAddressForm, addressModelLoaded} = useAddressModelRepository(formVisible, activeEmployeeUserName, hideForm);
+
     return (
-        <></>
+        <AddressCreateForm
+            formVisible={addressModelLoaded}
+            externalAddressRequestModel={addressModel}
+            addressCreateEndPointName="UpdateAddressByUserName"
+            activeEmployeeUserName={activeEmployeeUserName}
+            hideForm={hideAddressForm}
+            houseCreated={hideAddressForm}>
+        </AddressCreateForm>
     )
 }
 
