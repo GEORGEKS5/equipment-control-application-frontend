df28a09db49b7947c86211bb32f4c054a4a758ca
GOG <GOG@mail.ru>
2025-11-22 22:56:00 +0300

    added layout imports to superUserView

--- src/views/superUserView.jsx
+++ src/views/superUserView.jsx
@@ -1,3 +1,19 @@
+import dialogWindow from '../layouts/dialogWindow';
+import contentBlock from '../layouts/contentBlock';
+import contentBlockSection from '../layouts/contentBlockSection';
+import contentBlockSplitSection from '../layouts/contentBlockSplitSection';
+import contentSection from '../layouts/contentSection';
+import contentSplitSection from '../layouts/contentSplitSection';
+
+import FormField from '../layouts/slots/formField';
+import FormFooter from '../layouts/slots/formFooter';
+import MainContent from '../layouts/slots/mainContent';
+import SideBar from '../layouts/slots/sideBar';
+import SectionHeader from '../layouts/slots/sectionHeader';
+import SectionData from "../layouts/slots/sectionData";
+import SectionFooter from '../layouts/slots/sectionFooter';
+import ContentSectionFooter from "../layouts/slots/contentSectionFooter";
+
 function SuperUserView(){
     return (
         <>
