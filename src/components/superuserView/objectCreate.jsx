ca91ef1e2606e2b74ee99c847d9a985f93cb2f27
GOG <GOG@mail.ru>
2025-11-24 11:07:40 +0300

    added imports to objectCreate component

--- src/components/superuserView/objectCreate.jsx
+++ src/components/superuserView/objectCreate.jsx
@@ -1,3 +1,8 @@
+import DialogWindow from '../../layouts/dialogWindow';
+import FormInput from '../UI/formInput';
+import DefaultButton from '../UI/defaultButton';
+import getRequestPromise from '../../helpers/lib';
+
 function ObjectCreate({}) {
     return (
         <></>
