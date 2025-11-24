6634fbeedb524ee51c7b4718be3939b567eb6948
GOG <GOG@mail.ru>
2025-11-24 11:13:44 +0300

    added data and methods to objectCreate component

--- src/components/superuserView/objectCreate.jsx
+++ src/components/superuserView/objectCreate.jsx
@@ -2,8 +2,34 @@ import DialogWindow from '../../layouts/dialogWindow';
 import FormInput from '../UI/formInput';
 import DefaultButton from '../UI/defaultButton';
 import getRequestPromise from '../../helpers/lib';
+import { useContext, useState } from 'react';
+import UserContext from '../../context/user';
 
-function ObjectCreate({}) {
+function ObjectCreate({formVisible, constructiveObjectCreated, hideForm}) {
+    const [constructiveObjectName, setConstructiveObjectName] = useState('');
+    const {USER_STATE} = useContext(UserContext);
+
+    function setObjectName(value){
+        setConstructiveObjectName(value);
+    }
+
+    function sendCreationRequest(){
+        const servUrlAdr = USER_STATE.getServerUrlAddress();
+        const endPointName = 'InsertConstructiveObject';
+
+        const requestPromise = getRequestPromise(servUrlAdr, endPointName, { constructiveObjectName });
+
+        requestPromise.then(resp=>{
+            //setConstructiveObjectName('');
+            
+            if(resp.ok){
+                constructiveObjectCreated();
+            }else{   
+                hideForm();
+            }
+        })
+    }
+
     return (
         <></>
     )
