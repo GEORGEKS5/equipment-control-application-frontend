c22c28e7031dcbe9fadcf1c481bf975d2924603f
GOG <GOG@mail.ru>
2025-11-24 11:51:27 +0300

    added imports to supervisorAppointment component

--- src/components/superuserView/supervisorAppointment.jsx
+++ src/components/superuserView/supervisorAppointment.jsx
@@ -1,3 +1,11 @@
+import useEmployeeRepository from '../../hooks/useEmployeeRepository';
+import useEmployeeDataTable from '../../hooks/useEmployeeDataTable';
+import DialogView from '../../layouts/dialogWindow';
+import FormField from '../../layouts/slots/formField';
+import FormFooter from '../../layouts/slots/formFooter';
+import dataTable from '../../components/table/dataTable';
+import getRequestPromise from '../../helpers/lib';
+
 function SupervisorAppointment() {
     return (
         <></>
