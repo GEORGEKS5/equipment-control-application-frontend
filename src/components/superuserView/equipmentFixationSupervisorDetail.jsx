28dde8a789ece86f0838312653880ae2ae21920a
GOG <GOG@mail.ru>
2025-11-24 11:02:26 +0300

    adapted equipmentFixationSupervisorDetail component

--- src/components/superuserView/equipmentFixationSupervisorDetail.jsx
+++ src/components/superuserView/equipmentFixationSupervisorDetail.jsx
@@ -1,10 +1,38 @@
+import FormField from '../../layouts/slots/formField';
 import DialogWindow from '../../layouts/dialogWindow';
 import FormInput from '../../components/UI/formInput';
 import useEquipmentFixationSupervisorRepository from '../../hooks/useEquipmentFixationSupervisorRepository';
 
-function EquipmentFixationSupervisorDetail({}) {
+function EquipmentFixationSupervisorDetail({formVisible, activeFixationSupervisor, hideForm}) {
+    const {supervisorRepository} = useEquipmentFixationSupervisorRepository(formVisible, activeFixationSupervisor);
+
     return (
-        <></>
+        <DialogWindow
+            visibleForm={formVisible}
+            hideForm={hideForm}>
+            <FormField>
+                <FormInput
+                    inputValue={supervisorRepository.EmployeeLastName}
+                    fieldCaption="Фамилия"
+                    displayOnly={true}>
+                </FormInput>
+                <FormInput
+                    inputValue={supervisorRepository.EmployeeName}
+                    fieldCaption="Имя"
+                    displayOnly={true}>
+                </FormInput>
+                <FormInput
+                    inputValue={supervisorRepository.EmployeeSurName}
+                    fieldCaption="Отчество"
+                    displayOnly={true}>
+                </FormInput>
+                <FormInput
+                    inputValue={supervisorRepository.AppointmentDate}
+                    fieldCaption="Дата назначения"
+                    displayOnly={true}>
+                </FormInput>
+            </FormField>
+        </DialogWindow>
     )
 }
 
