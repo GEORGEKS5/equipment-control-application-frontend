8df699c2d4af10c24b274b5d402ce924f189b013
GOG <GOG@mail.ru>
2025-12-02 20:06:03 +0300

    adapted userView for project

--- src/views/userView.tsx
+++ src/views/userView.tsx
@@ -1,7 +1,69 @@
+import unauthAccessBlock from '../views/authErrorView';
+import getRequestPromise from '../helpers/lib';
+import HeaderBlock from '../components/headerBlock';
+import ContentSingleBlock from '../layouts/ContentSingleBlock';
+import ContentSection from '../layouts/contentSection';
+import TableColumnHeader from '../helpers/tableColumnHeader';
+import { useContext, useEffect, useState } from 'react';
+import UserContext from '../context/user';
+import { TUserContext } from '../helpers/types';
+import ContentBlockSection from '../layouts/contentBlockSection';
+import SectionData from "../layouts/slots/sectionData";
+import SectionHeader from "../layouts/slots/sectionHeader";
+import DataTable from '../components/table/dataTable';
+
 function UserView(){
+    const {USER_STATE} = useContext<{USER_STATE: TUserContext}>(UserContext);
+    const [fixedEquipment, setFixedEquipment] = useState<unknown[]>([]);
+    const [fixedEquipmentTableHeader, setFixedEquipmentTableHeader] = useState<unknown[]>([]);
+
+    function createFixedEquipmentTableHeader(){
+        const tableHeader = [];
+
+        tableHeader.push(new TableColumnHeader('FixationDate','Дата'))
+        tableHeader.push(new TableColumnHeader('CategoryName','Категория'))
+        tableHeader.push(new TableColumnHeader('BrandName','Производитель'))
+        tableHeader.push(new TableColumnHeader('ModelName','Модель'))
+        tableHeader.push(new TableColumnHeader('EmployeeLastName','Имя ответственого'))
+        tableHeader.push(new TableColumnHeader('EmployeeName','Фамилия ответственого'))
+        tableHeader.push(new TableColumnHeader('ObjectName','Объект'))
+
+        setFixedEquipmentTableHeader(tableHeader);
+    }
+
+    useEffect(() => {
+        let servAdr = USER_STATE.getServerUrlAddress();
+        let fPromise = getRequestPromise(servAdr, 'GetFixationsByEmpsKey', {EmpKey: USER_STATE.userName});
+
+        fPromise.then(res=>{
+            res.json().then(r=>{
+                console.log(r);
+                createFixedEquipmentTableHeader();
+                setFixedEquipment(r);
+            });
+        })
+    }, [])
+
     return (
         <>
-            <h1>UserView</h1>
+            <HeaderBlock></HeaderBlock>
+            <ContentSingleBlock>
+                <ContentBlockSection>
+                    <ContentSection>
+                        <SectionHeader>
+                            <h3>Закрепленая техника</h3>
+                        </SectionHeader>
+                        <SectionData>
+                            <DataTable
+                                tableData={fixedEquipment}
+                                tableStructure={fixedEquipmentTableHeader}
+                                tableActionButton={[]}
+                                buttonClick={()=>{}}>
+                            </DataTable>
+                        </SectionData>
+                    </ContentSection>
+                </ContentBlockSection>
+            </ContentSingleBlock>
         </>
     )
 }
